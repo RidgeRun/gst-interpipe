7e29e8b2c0f0949669f9069b0d419456c500a23e
Mguel Taylor <miguel.taylor@ridgerun.com>
2018-07-24 19:21:36 -0600

    Fix invalid caps test

--- tests/check/gst/test_invalid_caps.c
+++ tests/check/gst/test_invalid_caps.c
@@ -21,7 +21,6 @@
 #include "config.h"
 #endif
 
-#include <unistd.h>
 #include <gst/check/gstcheck.h>
 #include <gst/video/gstvideometa.h>
 #include <gst/app/gstappsrc.h>
@@ -48,29 +47,27 @@ GST_START_TEST (invalid_caps)
       ("videotestsrc name=vtsrc ! capsfilter caps=video/x-raw,width=640,height=480,framerate=(fraction)30/1 ! interpipesink "
           "name=intersink sync=true", &error));
   fail_if (error);
-
   intersink = gst_bin_get_by_name (GST_BIN (sink), "intersink");
 
   /* Create one source pipeline */
-
   src =
       GST_PIPELINE (gst_parse_launch
       ("interpipesrc name=intersrc listen-to=intersink ! capsfilter caps=video/x-raw,format=(string)I420,width=320,height=240,framerate=(fraction)30/1 ! "
           "appsink name=asink async=false", &error));
   fail_if (error);
-
   intersrc = gst_bin_get_by_name (GST_BIN (src), "intersrc");
 
   /* Play the pipelines */
-  gst_element_change_state (GST_ELEMENT (sink),
-      GST_STATE_CHANGE_PAUSED_TO_PLAYING);
-  gst_element_change_state (GST_ELEMENT (src),
-      GST_STATE_CHANGE_PAUSED_TO_PLAYING);
+  gst_element_set_state (GST_ELEMENT (src), GST_STATE_PLAYING);
+  fail_if (GST_STATE_CHANGE_FAILURE == gst_element_get_state (GST_ELEMENT (src),
+          NULL, NULL, GST_CLOCK_TIME_NONE));
+  gst_element_set_state (GST_ELEMENT (sink), GST_STATE_PLAYING);
+  fail_if (GST_STATE_CHANGE_FAILURE ==
+      gst_element_get_state (GST_ELEMENT (sink), NULL, NULL,
+          GST_CLOCK_TIME_NONE));
 
   /* Verifies if interpipesink and interpipesrc have caps set
    */
-  sleep (1);
-
   caps1 = gst_app_src_get_caps (GST_APP_SRC (intersrc));
   fail_if (caps1);
 
@@ -78,8 +75,8 @@ GST_START_TEST (invalid_caps)
   fail_if (!caps2);
 
   /* Stop pipelines */
-  gst_element_change_state (GST_ELEMENT (sink), GST_STATE_CHANGE_READY_TO_NULL);
-  gst_element_change_state (GST_ELEMENT (src), GST_STATE_CHANGE_READY_TO_NULL);
+  gst_element_set_state (GST_ELEMENT (sink), GST_STATE_NULL);
+  gst_element_set_state (GST_ELEMENT (src), GST_STATE_NULL);
 
   /* Cleanup */
   g_object_unref (intersink);
